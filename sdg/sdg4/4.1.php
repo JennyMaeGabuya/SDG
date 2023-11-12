c5d3cda75dec9219c5b125eb834fb3d7d295ac7b
Jenny Mae A. Gabuya <jennymaegabuya8@gmail.com>
2023-11-12 18:50:09 +0800

    add head title

--- sdg/sdg4/4.1.php
+++ sdg/sdg4/4.1.php
@@ -33,6 +33,8 @@ if (isset($_POST['submit'])) {
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>SDG 4.1 | Research on Early Years ... Education</title>
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href= css/sidebar.css>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">

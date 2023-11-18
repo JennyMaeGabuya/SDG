82c2cf3e7c64b94dc3420c3b68f336df587bf4bd
Jenny Mae A. Gabuya <jennymaegabuya8@gmail.com>
2023-11-18 15:37:41 +0800

    sidebar names

--- sdg/sdg4/sidebar.php
+++ sdg/sdg4/sidebar.php
@@ -7,30 +7,35 @@
     padding-left: 20px; /* Adjust as needed */
 }
 
+.sidenav {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
 /* Show the nested dropdown when hovering over the parent link */
 .dropdown-container:hover .nested-dropdown {
     display: block;
 }
 </style>
-  <a href="../../index.php">Home</a>
-  <a href="index.php">Quality Education</a>
-  <a href="4.1.php">4.1</a>
-  <button class="dropdown-btn">4.2
+
+  <b><a href="../../index.php">Home</a></b>
+  <a href="index.php">SDG 4: Quality Education</a>
+  <a href="4.1.php">4.1 Research Early Years </a>
+  <button class="dropdown-btn">4.2 Graduates
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="4.2.1.php">4.2.1</a>
-    <a href="4.2.2.php">4.2.2</a>
+    <a href="4.2.1.php">4.2.1 Resepective Field</a>
+    <a href="4.2.2.php">4.2.2 Primary Level</a>
   </div>
-  <button class="dropdown-btn">4.3
+  <button class="dropdown-btn">4.3 Learning Measures
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="4.3.1.php">4.3.1</a>
-    <a href="4.3.2.php">4.3.2</a>
-    <a href="4.3.3.php">4.3.3</a>
-    <a href="4.3.4.php">4.3.4</a>
-    <a href="4.3.5.php">4.3.5</a>
+    <a href="4.3.1.php">4.3.1 Public Resources</a>
+    <a href="4.3.2.php">4.3.2 Public Events</a>
+    <a href="4.3.3.php">4.3.3 Vocational Events</a>
+    <a href="4.3.4.php">4.3.4 Outreach Activity</a>
+    <a href="4.3.5.php">4.3.5 Learning Access</a>
   </div>
-  <a href="4.4.php">4.4</a>
+  <a href="4.4.php">4.4 First-Generation Student</a>
 </div>

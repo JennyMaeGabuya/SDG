d1f36438f094dac5506a5ca5b95141fa3cb4394c
erikamagnaye <21-31350@g.batstate-u.edu.ph>
2023-11-24 13:45:51 +0800

    may prob pa

--- sdg/sdg15/15.1.php
+++ sdg/sdg15/15.1.php
@@ -176,7 +176,6 @@ h2 {
        if (!$result) {
          die("Error: " . mysqli_error($conn)); // Output the error message for debugging
        }
-   
        // Initializing total points outside the loop
        $totalPoints = 0;
    
@@ -187,6 +186,7 @@ h2 {
          // Define source for different categories
        $src = "SCOPUS";
        $osrc = "WEB OF SCIENCE";
+      }
        $ptsR = 0; // Initialize points for the current research
    
        // Check if the source matches SCOPUS or WEB OF SCIENCE
@@ -195,15 +195,9 @@ h2 {
        } else {
          $ptsR = $totalno * 10;
        }
-     // Add the points for the current research to the total points
-     $totalPoints += $ptsR;
-     
-       }
- 
-     
+        // Add the points for the current research to the total points
+        $totalPoints += $ptsR;
     }
-
-  
     ?>
 
 <input type="text" style="color: black; text-align: center;  " class="points" name="points" value="<?php echo min($totalPoints, 25); ?>" readonly>  

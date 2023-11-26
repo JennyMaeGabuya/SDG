7d3ec84b9d9c9e3b8fb0b992be7ee24394ae99b8
JennyMaeGabuya <jennymaegabuya8@gmail.com>
2023-11-27 03:29:16 +0800

    table not responding to size, hays

---
+++ sdg/sdg15/15.2.1.1.php
@@ -0,0 +1,459 @@
+<!--============================================================================================= 
+                      INSERT DATA TO DB ONCE SUBMIT BUTTON WAS CLICKED
+============================================================================================= -->
+<?php
+include "includes/config.php"; // CHANGE THIS WITH YOUR ACTUAL CONNECTION TO DATABASE ex: conn.php
+
+if (isset($_POST['submit'])) {
+    // Get user inputs
+    $totalStudents = $_POST['totalStudents'];
+    $maleSpaceForGardening = $_POST['maleSpaceForGardening'];
+    $femaleSpaceForGardening = $_POST['femaleSpaceForGardening'];
+    $maleSpaceForVerticalGardening = $_POST['maleSpaceForVerticalGardening'];
+    $femaleSpaceForVerticalGardening = $_POST['femaleSpaceForVerticalGardening'];
+
+    // Insert data into the database
+    $sql = "INSERT INTO tbl15_2_student (total_students, mgardening, fgardening, mvertical, fvertical)
+            VALUES ('$totalStudents', '$maleSpaceForGardening', '$femaleSpaceForGardening', '$maleSpaceForVerticalGardening', '$femaleSpaceForVerticalGardening')";
+
+    if ($conn->query($sql) === TRUE) {
+        $successMessage = "You have successfully entered data";
+        // Display success message using JavaScript alert
+        echo "<script>
+                swal({
+                    title: 'Success',
+                    text: '$successMessage',
+                    icon: 'success',
+                    button: 'OK'
+                });
+            </script>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SDG 15.2 | Sustainable Land Practices</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href=css/sidebar.css>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- for icons */  -->
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+    <!--- for table __-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <style>
+        body {
+            font-family: "Lato", sans-serif;
+            background-color: white;
+        }
+
+        /* Main content */
+        .main {
+            /* margin-left: 250px;  Same as the width of the sidenav */
+            font-size: 20px;
+            /* Increased text to enable scrolling */
+            padding: 0px 10px;
+            color: #FCF5ED;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #56c02b;
+            font-family: Verdana, sans-serif;
+            font-weight: bolder;
+            /* ==========================================just added */
+        }
+
+        .points {
+            width: 60px;
+            height: 40px;
+            border-radius: 15px;
+        }
+
+        p {
+            margin: 20px;
+            font-size: 16px;
+        }
+
+        h2 {
+            flex: 1;
+            /* Allow h2 to grow to take available space */
+        }
+
+        .input-container {
+            display: flex;
+            align-items: center;
+        }
+
+        p {
+            margin-right: 10px;
+        }
+
+        .content {
+            /* Same as the width of the sidenav */
+            font-size: 17px;
+            /* Increased text to enable scrolling */
+            padding: 0px 10px;
+            color: #C31F33;
+            margin-left: 80px;
+        }
+
+        .card {
+            width: 70%;
+            margin-left: 350px;
+            font-size: 17px;
+            margin-bottom: 50px;
+            margin-top: 20px;
+            height: auto;
+        }
+
+        .content>p {
+            text-align: center;
+            justify-content: justify-content;
+            font-size: 14px;
+            margin: auto;
+        }
+
+        .form-control {
+            margin-right: 30px;
+            display: block;
+            width: 88%;
+            height: 30px;
+        }
+
+        .form-row {
+
+            height: 30px;
+
+        }
+
+        .contentform {
+            margin-top: 30px;
+            margin-left: 50px;
+            margin-bottom: 30px;
+            justify-content: center;
+            align-items: center;
+
+        }
+
+        .btn {
+            margin-bottom: 10px;
+
+        }
+
+        .table-container {
+            margin-right: 20px;
+            width: 70%;
+            overflow-x: auto;
+        }
+
+        .h3text {
+            justify-content: center;
+
+        }
+
+        .points {
+            box-shadow: grey;
+        }
+
+        .fields {
+            border-bottom: solid 1px #e9d6d6;
+            margin-right: 25px;
+        }
+
+        /* css for confrim button */
+        .button-container {
+            display: flex;
+            align-items: center;
+            /* Align items vertically */
+        }
+
+        .btn {
+            margin-right: 10px;
+            /* Adjust margin as needed */
+        }
+
+        .confirmation-text {
+            margin: 0;
+            /* Remove default margin for the paragraph */
+        }
+
+        /* endddd */
+
+        /* ==========================================just added===================================================== */
+        /* Centering content and responsiveness */
+        .card {
+            width: 55%;
+            /* Adjust card width */
+            margin: 60px auto;
+            /* Center the card and provide space */
+            font-size: 16px;
+        }
+
+        .table-container {
+            width: 100%;
+            /* Adjust table container width */
+            margin: 20px auto;
+            /* Center the table and provide space */
+            overflow-x: auto;
+            /* Enable horizontal scrolling on smaller screens if needed */
+            font-size: 15px;
+        }
+
+        /* Media query for smaller screens */
+        @media only screen and (max-width: 768px) {
+            .card {
+                width: 90%;
+                /* Adjust card width for smaller screens */
+                margin: 10px auto;
+                /* Adjust margin for smaller screens */
+                font-size: 13px;
+            }
+
+            .table-container {
+                width: 90%;
+                /* Adjust table container width for smaller screens */
+                margin: 10px auto;
+                font-size: 13px;
+            }
+
+            .content {
+                /* Same as the width of the sidenav */
+                font-size: 16px;
+                /* Increased text to enable scrolling */
+                padding: 0px 10px;
+                color: #C31F33;
+                margin-left: 5px;
+            }
+        }
+    </style>
+
+<body>
+    <?php include('sidebar.php'); ?>
+
+
+    <!--============================================================================================= 
+                                  TITINGNAN KO PA MUNA YUNG SUNOD
+
+    <div class="main">
+        <h2 class="text-center">SDG 15 LIFE ON LAND</h2>
+        <div class="input-container">
+            <p>Points</p>
+
+            <?php
+            include "includes/config.php";
+            $query = "SELECT Count(*) AS total FROM `tbl15_41_events`"; // SQL query to fetch all table data
+            $result = mysqli_query($conn, $query); // sending the query to the database
+
+            $totalPoints = 0;
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $total = $row['total'];
+                $x = $total;
+
+                // calculate points by dividing total number of PPAs by 2 and multiply by 5
+                $points = ($x / 2 * 15);
+
+
+                // Add the points  to the total points
+                $totalPoints += $points;
+            }
+            ?>
+
+            <input type="text" style="color: black; text-align: center;" class="points" name="points" value="<?php echo min($totalPoints, 15); ?>" readonly>
+        </div>
+    </div>
+============================================================================================= -->
+
+    <!--============================================================================================= 
+                                  END OF HEADER POINTING SYSTEM 
+ ============================================================================================= -->
+
+
+    <div class="content">
+        <br>
+        <h3 class="text-center">15.2.1 Measures That Promote Sustainable Land Practices</h3>
+        <p>Have programs, projects or activities that promotes sustainable<br> land practices </p>
+
+        <!--============================================================================================= 
+                                  START OF FORM
+============================================================================================= -->
+        <div class="card">
+            <div class="contentform">
+                <form method="POST">
+
+                    <div class="form-group">
+                        <label class="text-center">Total number of students</label>
+                        <input type="number" class="form-control" name="totalStudents" required>
+                    </div>
+
+                    <fieldset>
+                        <legend>Gardening Space</legend>
+
+                        <div class="form-group">
+                            <label>Male students with available space for gardening</label>
+                            <input type="number" class="form-control" name="maleSpaceForGardening" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Female students with available space for gardening</label>
+                            <input type="number" class="form-control" name="femaleSpaceForGardening" required>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Vertical Gardening</legend>
+
+                        <div class="form-group">
+                            <label>Male students with available space for vertical gardening</label>
+                            <input type="number" class="form-control" name="maleSpaceForVerticalGardening" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Female students with available space for vertical gardening</label>
+                            <input type="number" class="form-control" name="femaleSpaceForVerticalGardening" required>
+                        </div>
+                    </fieldset>
+
+                </form>
+
+                <?php
+                if (isset($successMessage)) {
+                    echo "<script>
+                    swal({
+                        title: 'Success',
+                        text: '$successMessage',
+                        icon: 'success',
+                        button: 'OK'
+                    });
+                </script>";
+                }
+                ?>
+
+                <button type="submit" class="btn btn-primary mb-3 me-1" name="submit">
+                    <i class="fa fa-send"></i> Submit
+                </button>
+
+                <button type="reset" class="btn btn-danger mb-3" name="cancel">
+                    <i class="fa fa-times-circle"></i> Cancel
+                </button>
+
+            </div><br>
+
+            </form>
+        </div>
+    </div>
+    <!--============================================================================================= 
+                                END OF FORM
+============================================================================================= -->
+
+    <!--============================================================================================= 
+                        START OF TABLE/ DISPLAY ALL RECORDS FROM DATABASE
+============================================================================================= -->
+    <div class="table-container">
+        <h2 class="text-center">Sustainable Land Practices</h2>
+        <table class="table table-bordered" style="margin: auto;width:56%;">
+            <thead>
+                <tr>
+                    <th scope="col" style="width: 70px;">Total Students</th>
+                    <th scope="col" style="width: 70px;">Male Gardening Space</th>
+                    <th scope="col" style="width: 70px;">Female Gardening Space</th>
+                    <th scope="col" style="width: 20px;"> % Gardening Space</th>
+                    <th scope="col" style="width: 70px;">Male Vertical Gardening Space</th>
+                    <th scope="col" style="width: 70px;">Female Vertical Gardening Space</th>
+                    <th scope="col" style="width: 20px;"> % Vertical Gardening</th>
+                    <th scope="col" colspan="2" class="text-center" style="width: 40px;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include "includes/config.php";
+
+                $query = "SELECT * FROM `tbl15_2_student` ORDER BY `ID` DESC"; // Fetching data from tbl15_2_student table
+                $result = mysqli_query($conn, $query);
+
+                if (!$result) {
+                    die("Error: " . mysqli_error($conn));
+                }
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $totalStudents = $row['total_students'];
+                    $maleGardening = $row['mgardening'];
+                    $femaleGardening = $row['fgardening'];
+                    $maleVertical = $row['mvertical'];
+                    $femaleVertical = $row['fvertical'];
+
+                    // Calculate total students involved in gardening and vertical gardening
+                    $totalGardeningStudents = $maleGardening + $femaleGardening;
+                    $totalVerticalStudents = $maleVertical + $femaleVertical;
+
+                    echo "<tr>";
+                    echo "<td>{$totalStudents}</td>";
+                    echo "<td>{$maleGardening}</td>";
+                    echo "<td>{$femaleGardening}</td>";
+                    echo "<td>" . round(($totalGardeningStudents / $totalStudents) * 100, 2) . "%</td>";
+                    echo "<td>{$maleVertical}</td>";
+                    echo "<td>{$femaleVertical}</td>";
+                    echo "<td>" . round(($totalVerticalStudents / $totalStudents) * 100, 2) . "%</td>";
+                    echo "</tr>";
+
+                    /* echo "<td>$points</td>";
+
+                    echo "<td class='text-center' style='width:40px'>
+                <a href='edit/edit15.4.1.php?update&event_id={$id}' style='width:30px;'>
+                    <i class='fa fa-edit'></i>
+                </a>
+                <a href='delete/delete15.4.1.php?delete={$id}' style='width:30px;color:red;'>
+                    <i class='fa fa-trash'></i>
+                </a>
+            </td>";   */
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    </div>
+
+    <!--============================================================================================= 
+                                END OF TABLE
+============================================================================================= -->
+
+    <!--============================================================================================= 
+//                           this is js for sidebar panel
+// DONT REMOVE IT. MAKE SURE TO INCLUDE IT TO ALL YOUR FILES   !!!!!!!!!!
+//============================================================================================= -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        //this is js for sidebar panel
+        /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "block";
+                }
+            });
+        }
+    </script>
+
+</body>
+
+</html>
--- sdg/sdg15/sidebar.php
+++ sdg/sdg15/sidebar.php
@@ -1,60 +1,67 @@
 <style>
-/* Hamburger icon styles */
+    /* Hamburger icon styles */
+    .hamburger {
+        display: none;
+        /* Initially hide the hamburger icon */
+        cursor: pointer;
+        padding: 10px;
+    }
+
+    /* Media query for smaller screens */
+    @media screen and (max-width: 768px) {
+        .sidenav {
+            width: 100%;
+            position: fixed;
+            top: 0;
+            left: -100%;
+            transition: left 0.3s ease;
+        }
+
+        .sidenav.active {
+            left: 0;
+        }
+
+        /* Show the hamburger icon when the screen size is small */
         .hamburger {
-            display: none; /* Initially hide the hamburger icon */
-            cursor: pointer;
-            padding: 10px;
+            display: block;
         }
-
-        /* Media query for smaller screens */
-        @media screen and (max-width: 768px) {
-            .sidenav {
-                width: 100%;
-                position: fixed;
-                top: 0;
-                left: -100%;
-                transition: left 0.3s ease;
-            }
-
-            .sidenav.active {
-                left: 0;
-            }
-
-            /* Show the hamburger icon when the screen size is small */
-            .hamburger {
-                display: block;
-            }
-        }
-        
-    </style>
+    }
+</style>
 </head>
+
 <body>
 
-<!-- Your existing sidebar code -->
-<div class="sidenav">
-    <div class="hamburger" onclick="toggleSidebar()">
-        &#9776; <!-- Hamburger icon -->
+    <!-- Your existing sidebar code -->
+    <div class="sidenav">
+        <div class="hamburger" onclick="toggleSidebar()">
+            &#9776; <!-- Hamburger icon -->
+        </div>
+        <img src="img/15.png" class="img logo" alt="logo" style="margin-top: 50px;">
+        <b><a href="../../index.php">Home</a></b>
+        <a href="index.php">SDG 15: Life on Land</a>
+        <a href="15.1.php">15.1 Research on Land</a>
+        <button class="dropdown-btn">15.2 Sustainable Land ..
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container">
+            <a href="15.2.1.1.php">15.2.1.1 Students</a>
+                <a href="15.2.1.2.php">15.2.1.2 Employees</a>
+        </div>
+        <a href="15.3.php">15.3 Endangered Flora/Fauna</a>
+        <button class="dropdown-btn">15.4 Sustainable Use ..
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container">
+            <a href="15.4.1.php">15.4.1 Sustainable Use of Land</a>
+            <a href="15.4.2.php">15.4.2 Farmed Food</a>
+            <a href="15.4.3.php">15.4.3 Ecosystem's Biodiversity</a>
+        </div>
+        <a href="15.5.1.php">15.5.1 Biodiversity</a>
     </div>
-    <img src="img/15.png" class="img logo" alt="logo" style="margin-top: 50px;" >
-    <b><a href="../../index.php">Home</a></b>
-    <a href="index.php">SDG 15: Life on Land</a>
-    <a href="15.1.php">15.1 Research on Land</a>
-    <a href="15.2.php">15.2 Land Practices</a>
-    <a href="15.3.php">15.3 Endangered Flora/Fauna</a>
-    <button class="dropdown-btn">15.4 Sustainable Use ..
-        <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-container">
-        <a href="15.4.1.php">15.4.1 Sustainable Use of Land</a>
-        <a href="15.4.2.php">15.4.2 Farmed Food</a>
-        <a href="15.4.3.php">15.4.3 Ecosystem's Biodiversity</a>
-    </div>
-    <a href="15.5.1.php">15.5.1 Biodiversity</a>
-</div>
 
-<script>
-    function toggleSidebar() {
-        const sidebar = document.querySelector('.sidenav');
-        sidebar.classList.toggle('active');
-    }
-</script>
+    <script>
+        function toggleSidebar() {
+            const sidebar = document.querySelector('.sidenav');
+            sidebar.classList.toggle('active');
+        }
+    </script>
